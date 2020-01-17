75aa1724deba6b1190be98d7dd8fd69969a5e224
mpira <mpira@esriaustralia.com.au>
2020-01-17 13:24:22 +0800

    Commit

---
+++ MakePDF.py
@@ -0,0 +1,32 @@
+﻿import arcpy,os
+
+# Enable overwriting of outputs
+arcpy.env.overwriteOutput = True
+
+# Project object
+aprx = arcpy.mp.ArcGISProject(
+    r"C:\LPA\Projects\LayoutProject\LayoutProject.aprx")
+
+# Map object
+mapx = aprx.listMaps("Map")[0]
+
+# Layer object
+countriesLayer = mapx.listLayers("Countries")[0]
+
+# Layout object
+lyt = aprx.listLayouts()[0]
+
+# Map Frame object
+mapFrame = lyt.listElements('MAPFRAME_ELEMENT',"Map Frame")[0]
+mapFrame.elementWidth = lyt.pageWidth - 20
+mapFrame.elementHeight = mapFrame.elementWidth
+mapFrame.elementPositionX = 10
+mapFrame.elementPositionY = lyt.pageHeight / 4
+
+# Export to PDF and open in Adobe Acrobat Reader
+lyt.exportToPDF(r"C:\LPA\PDFs\test.pdf")
+os.startfile(r"C:\LPA\PDFs\test.pdf")
+
+# Delete project object
+del aprx
+
